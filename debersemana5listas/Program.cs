b81cf9b87d527f080e1dae3753d6d51b9965bf60
¨the <ma.pazminoa@uea.edu.ec>
2025-01-23 22:39:20 -0500

    first commit

---
+++ debersemana5listas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Crear una lista de asignaturas
+        List<string> asignaturas = new List<string>();
+
+        // Agregar asignaturas a la lista
+        asignaturas.Add("Matemáticas");
+        asignaturas.Add("Física");
+        asignaturas.Add("Química");
+        asignaturas.Add("Historia");
+        asignaturas.Add("Lengua");
+
+        // Mostrar las asignaturas por pantalla
+        Console.WriteLine("Asignaturas del curso:");
+        foreach (string asignatura in asignaturas)
+        {
+            Console.WriteLine(asignatura);
+        }
+    }
+}
